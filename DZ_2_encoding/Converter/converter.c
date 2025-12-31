3f4756d5d8ec9fab215daf29ed7c86771c8e5df1
babnikolay <babnikolay@mail.ru>
2025-12-31 08:05:24 +0300

    Changed size of file

--- DZ_2_encoding/Converter/converter.c
+++ DZ_2_encoding/Converter/converter.c
@@ -204,8 +204,8 @@ int find_element_index(unsigned char arr[ROWS][COLS], enum enc_t encoding, unsig
 
 void converting_to_utf8(FILE *input, enum enc_t encoding, FILE *output) {
     int file_len = 0;
-    while (!feof(input)) {
-        getc(input);
+    while (getc(input) != EOF) {
+        // getc(input);
         // char ch = getc(input);
         // printf("%c", ch);
         file_len++;
